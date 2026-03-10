ea25bc3a94a3c07d919290f533e28169d656f2e7
tammo161 <t.pientak@posteo.de>
2026-03-10 14:04:39 +0100

    JSON angelegt und Javascript geschrieben(versucht)

--- function.js
+++ function.js
@@ -1,1 +1,10 @@
-const speisen = ['Seelachs','Hackschnitzel', 'Westfälische Beutelwurst'];
+function loadMeals() {
+  console.log("in loadMeals");
+
+  const speisen = ["Seelachs", "Hackschnitzel", "Westfälische Beutelwurst"];
+  const mealId = ["#essen1", "#essen2", "#essen3"];
+
+  for (let i = 0; i < speisen.length; i++) {
+    for (let j = 0; j < mealId.length; j++) u(mealId[j]).replace(speisen[j]);
+  }
+}
