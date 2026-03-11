967e36f18a665b7787a06f05148aa245953bbc60
tammo161 <t.pientak@posteo.de>
2026-03-11 14:01:07 +0100

    Loop für Essensplatzierung gebaut und json weiter geschrieben

--- function.js
+++ function.js
@@ -1,10 +1,43 @@
 function loadMeals() {
   console.log("in loadMeals");
+  
+ const json =[
+  {
+    "ausgabe": "Ausgabe 3",
+    "essen": "Westfälische Beutelwurst"
+  },
+  {
+    "ausgabe": "Ausgabe 1",
+    "essen": "Seelachs"
+  },
+  {
+    "ausgabe": "Burger",
+    "essen": "Black Burger mit Pulled Salmon"
+  },
+   {
+    "ausgabe": "Ausgabe 2",
+    "essen": "Hackschnitzel"
+  },
+];
 
-  const speisen = ["Seelachs", "Hackschnitzel", "Westfälische Beutelwurst"];
-  const mealId = ["#essen1", "#essen2", "#essen3"];
-
-  for (let i = 0; i < speisen.length; i++) {
-    for (let j = 0; j < mealId.length; j++) u(mealId[j]).replace(speisen[j]);
+  for(let i = 0; i <= json.length; i++){
+     if(json[i].ausgabe === "Ausgabe 1"){
+       u('#ausgabe1').replace(json[i].ausgabe);
+       u('#essen1').replace(json[i].essen);
+     }
+     if(json[i].ausgabe === "Ausgabe 2"){
+       u('#ausgabe2').replace(json[i].ausgabe);
+       u('#essen2').replace(json[i].essen);
+     }
+      if(json[i].ausgabe === "Ausgabe 3"){
+       u('#ausgabe3').replace(json[i].ausgabe);
+       u('#essen3').replace(json[i].essen);
+     }
+      if(json[i].ausgabe === "Burger"){
+       u('#burgerDerWoche').replace(json[i].ausgabe);
+       u('#burger').replace(json[i].essen);
+     }
   }
+  
 }
+
