29ebe639967fa1ed842cb39e78bdd2a690aff3b5
tammo161 <t.pientak@posteo.de>
2026-03-12 13:40:15 +0100

    Loop für Essenseintragung optimiert

--- function.js
+++ function.js
@@ -1,43 +1,36 @@
+const json = JSON.parse(fs.readFileSsync("mensaEssen.json"));
+
 function loadMeals() {
   console.log("in loadMeals");
-  
- const json =[
-  {
-    "ausgabe": "Ausgabe 3",
-    "essen": "Westfälische Beutelwurst"
-  },
-  {
-    "ausgabe": "Ausgabe 1",
-    "essen": "Seelachs"
-  },
-  {
-    "ausgabe": "Burger",
-    "essen": "Black Burger mit Pulled Salmon"
-  },
-   {
-    "ausgabe": "Ausgabe 2",
-    "essen": "Hackschnitzel"
-  },
-];
 
-  for(let i = 0; i <= json.length; i++){
-     if(json[i].ausgabe === "Ausgabe 1"){
-       u('#ausgabe1').replace(json[i].ausgabe);
-       u('#essen1').replace(json[i].essen);
-     }
-     if(json[i].ausgabe === "Ausgabe 2"){
-       u('#ausgabe2').replace(json[i].ausgabe);
-       u('#essen2').replace(json[i].essen);
-     }
-      if(json[i].ausgabe === "Ausgabe 3"){
-       u('#ausgabe3').replace(json[i].ausgabe);
-       u('#essen3').replace(json[i].essen);
-     }
-      if(json[i].ausgabe === "Burger"){
-       u('#burgerDerWoche').replace(json[i].ausgabe);
-       u('#burger').replace(json[i].essen);
-     }
+  const json = [
+    {
+      counterId: "counter3",
+      counterName: "Ausgabe 3",
+      essen: "Westfälische Beutelwurst",
+    },
+    {
+      counterId: "counter1",
+      counterName: "Ausgabe 1",
+      essen: "Seelachs",
+    },
+    {
+      counterId: "counter4",
+      counterName: "Burger",
+      essen: "Black Burger mit Pulled Salmon",
+    },
+    {
+      counterId: "counter2",
+      counterName: "Ausgabe 2",
+      essen: "Hackschnitzel",
+    },
+  ];
+
+  for (let i = 0; i <= json.length - 1; i++) {
+    const counter = json[i];
+    const counterId = counter.counterId;
+
+    u("#" + counterId + "-name").replace(counter.counterName);
+    u("#" + counterId + "-food").replace(counter.essen);
   }
-  
 }
-
