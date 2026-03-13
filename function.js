4ed259a6c1571fb0879ab88c6b1d524b4eae60fa
tammo161 <t.pientak@posteo.de>
2026-03-13 14:00:42 +0100

    Likecount in localStorage eintragen angefangen

--- function.js
+++ function.js
@@ -1,4 +1,5 @@
-const json = JSON.parse(fs.readFileSsync("mensaEssen.json"));
+
+
 
 function loadMeals() {
   console.log("in loadMeals");
@@ -24,7 +25,7 @@ function loadMeals() {
       counterName: "Ausgabe 2",
       essen: "Hackschnitzel",
     },
-  ];
+  ]; 
 
   for (let i = 0; i <= json.length - 1; i++) {
     const counter = json[i];
@@ -34,3 +35,23 @@ function loadMeals() {
     u("#" + counterId + "-food").replace(counter.essen);
   }
 }
+
+function initialize(){
+
+let vote1 = Number.parseInt(window.localStorage.getItem("likecount"));
+
+  u('#btnLike1').on('click', function(e){
+  window.localStorage.setItem('likecount', vote ++)
+});
+
+  u('#btnLike2').on('click', function(e){
+  window.localStorage.setItem('likecount', '2')
+});
+}
+
+
+
+
+
+  
+
